c68232e6d2dfd0ba2a5998cabe8694f150fafcf0
milisievert <36296782+milisievert@users.noreply.github.com>
2026-04-09 22:32:17 +0200

    feat(graph): graph improvements (#35)

--- libs/signals/src/lib/signals.spec.ts
+++ libs/signals/src/lib/signals.spec.ts
@@ -1,18 +1,20 @@
 import { signal } from './signal.js';
 import { isSignal } from './signals.js';
 
-describe('signals:isSignal', () => {
-  it('should return true for signals', () => {
-    const result = isSignal(signal(''));
-    expect(result).toBe(true);
-  });
+describe('signals', () => {
+  describe('isSignal', () => {
+    it('should return true for signals', () => {
+      const result = isSignal(signal(''));
+      expect(result).toBe(true);
+    });
 
-  it('should return false for other values', () => {
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    const result = ['', 0, {}, [], () => {}]
-      .map(isSignal)
-      .every((value) => value);
+    it('should return false for other values', () => {
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      const result = ['', 0, {}, [], () => {}]
+        .map(isSignal)
+        .every((value) => value);
 
-    expect(result).toBe(false);
+      expect(result).toBe(false);
+    });
   });
 });
